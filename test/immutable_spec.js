b1b6b1e27728fa95ee31b5203374759627b76745
Tony <anthony.c.young18@gmail.com>
2017-10-06 10:45:58 +0100

    added first tests for immutable checks

---
+++ test/immutable_spec.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import { List } from 'immutable';
+
+describe('immutability', () => {
+
+  describe('a number', () => {
+
+    function increment(currentState) {
+      return currentState + 1;
+    }
+
+    it('is immutable', () => {
+
+      let state = 42;
+      let nextState = increment(state);
+
+      expect(nextState).to.equal(43);
+      expect(state).to.equal(42);
+    });
+  });
+
+  describe('a list', () => {
+
+    function addMovie(currentState, movie) {
+
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+
+      let state = List.of('Trainspotting', '28 days later');
+      let nextState = addMovie(state, 'Terminator');
+
+      expect(nextState).to.equal(List.of('Trainspotting', '28 days later', 'Terminator'));
+      expect(state).to.equal(List.of('Trainspotting', '28 days later'));
+    })
+  })
+});
